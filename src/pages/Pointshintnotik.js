27550828a0d2d4c98e7bbb58005439b4079be39a
rahud <rahudnainggolan@gmail.com>
2025-05-19 12:17:49 +0700

    s

--- src/pages/Pointshintnotik.js
+++ src/pages/Pointshintnotik.js
@@ -3,6 +3,7 @@ import { Button, Modal } from 'react-bootstrap';
 import { FaPlay } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { HandlePageClick } from '../App'; // Import HandlePageClick from your App.js
+import './Pointshintnotik.css';
 
 const Pointshintnotik = ({ showModal, handleClose }) => {
   const navigate = useNavigate();
@@ -15,10 +16,10 @@ const Pointshintnotik = ({ showModal, handleClose }) => {
 
   return (
     <Modal show={showModal} onHide={handleClose}>
-      <Modal.Header closeButton>
+      <Modal.Header className='point-hint-notik' closeButton>
         <Modal.Title><strong>Boost Your Earnings with Notik!</strong></Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className='point-hint-notik'>
         <p>
           <strong>Get rewarded instantly!</strong> Earn <strong style={{ color: '#28a745' }}>$CUAN</strong> for every task you complete.
         </p>
@@ -26,7 +27,7 @@ const Pointshintnotik = ({ showModal, handleClose }) => {
           No complicated conversions—just real earnings! Start completing offers now and watch your <strong style={{ color: '#28a745' }}>$CUAN</strong> balance grow.
         </p>
       </Modal.Body>
-      <Modal.Footer className="d-flex justify-content-center">
+      <Modal.Footer className="d-flex justify-content-center point-hint-notik">
         <Button
           variant="primary"
           className="w-100 d-flex align-items-center justify-content-center"
